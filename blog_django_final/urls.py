bb10ffa1cc2f685f89cadcfc1384aec258818df6
TongasHub <ferreyragaston40@gmail.com>
2021-12-14 17:18:27 -0300

    vamos arreeglar esto porfa :3

---
+++ blog_django_final/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import TemplateView
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include(("posts.urls", "posts"), namespace="posts")),
+    path(
+        route="sobre-mi",
+        view=TemplateView.as_view(template_name="about.html"),
+        name="about",
+    ),
+    path("", include(("users.urls", "users"), namespace="users")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
